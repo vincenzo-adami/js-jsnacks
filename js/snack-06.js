f75c0053f954edf41f4fa7a8ac26d7ce3b0d53f2
vincenzo-adami <vincenzoadami92@gmail.com>
2024-07-11 19:09:05 +0200

    EDIT: change round method for random number

--- js/snack-06.js
+++ js/snack-06.js
@@ -1,6 +1,6 @@
 // Fate generare un numero random da 0 a 10 al computer, e chiedete all’utente di inserire un suo numero.Se il numero scelto dall’utente è uguale al numero del computer informate l’utente che ha vinto, altrimenti ha perso.
 
-let randomNumber = Math.round(Math.random() * 10); // intevallo 0-10, estremi compresi
+let randomNumber = Math.floor(Math.random() * 11); // intevallo 0-10, estremi compresi
 let userNumber = parseInt(prompt('Ti senti fortunato? Scrivi un numero intero'));
 
 if (randomNumber === userNumber) {
